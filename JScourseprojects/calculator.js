7db241ab89df82ebd3ab1b555982429f65415b54
vn-cheruiyot <cheruiyot.vincent.n@gmail.com>
2024-12-10 16:53:59 +0300

    styling calculator

--- JScourseprojects/calculator.js
+++ JScourseprojects/calculator.js
@@ -1,79 +1,16 @@
-// when a person clicks the buttons duisplay the values
-let statement = ``
-let end = 0
-let origin = location.origin;
-function myFunctionDot(){
-	statement += "."
-	document.getElementById("type-bar").innerHTML = statement
+let v = ""
+function mycalc(x){
+  v+=x;
+  document.getElementById("input").value = v;
 }
-function myFunction0(){
-	statement += "0"
-	document.getElementById("type-bar").innerHTML = statement
+function myeval(){
+  v = eval(v)
+  document.getElementById("input").value = v;
 }
-function myFunction1(){
-	statement += "1"
-	document.getElementById("type-bar").innerHTML = statement
+function myDel(){
+	v = v.slice(0,-1);
+	document.getElementById("input").value = v;
 }
-function myFunction2(){
-	statement += "2"
-	document.getElementById("type-bar").innerHTML = statement
+function mypercentage(){
+	v = eval(v)/100
 }
-function myFunction3(){
-	statement += "3"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction4(){
-	statement += "4"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction5(){
-	statement += "5"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction6(){
-	statement += "6"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction7(){
-	statement += "7"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction8(){
-	statement += "8"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunction9(){
-	statement += "9"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionAdd(){
-	statement += "+"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionMinus(){
-	statement += "-"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionMultiply(){
-	statement += "*"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionDivide(){
-	statement += "/"
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionEquals(){
-	let end= eval(statement)
-	document.getElementById("type-bar").innerHTML = end
-}
-function myFunctionOpen(){
-	statement += "("
-	document.getElementById("type-bar").innerHTML = statement
-}
-function myFunctionClose(){
-	statement += ")"
-	document.getElementById("type-bar").innerHTML = statement
-}
-
-
-
