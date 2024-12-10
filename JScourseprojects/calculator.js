49d0221aeb32536d0d51b2818af5317df334d9ce
vn-cheruiyot <cheruiyot.vincent.n@gmail.com>
2024-12-10 18:36:25 +0300

    Styling
    fixed answers to for decimal places
    introduced syntax error
    Added coming soon for scientific features

--- JScourseprojects/calculator.js
+++ JScourseprojects/calculator.js
@@ -4,13 +4,21 @@ function mycalc(x){
   document.getElementById("input").value = v;
 }
 function myeval(){
-  v = eval(v)
-  document.getElementById("input").value = v;
+  v = eval(v).toFixed(4)
+  if (isNaN(v)){
+    document.getElementById("input").value = "Syntax Error"; 
+  } else {
+   document.getElementById("input").value = v; 
+  } 
 }
 function myDel(){
 	v = v.slice(0,-1);
 	document.getElementById("input").value = v;
 }
 function mypercentage(){
-	v = eval(v)/100
+	v = eval(v)*100
+  document.getElementById("input").value = v;
+}
+function mysci(){
+  alert(`Coming Soon`)
 }
